26aec7d1e24475384ff1ad57e563b105ed0bc8e0
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2020-08-31 13:06:10 +0900

    docs: update

---
+++ doc/make_html.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+build_html()
+{
+    LANG=$1
+    if [ x${LANG} = x ]; then
+        LANG="en"
+    fi
+    cd source
+    sphinx-intl build -l "${LANG}"
+    sphinx-build -b html -a -E -D language="${LANG}" . ../_build/html/${LANG}
+}
+
+
+echo ">>>> build en"
+build_html en
+
+echo ">>>> build ja"
+build_html ja
