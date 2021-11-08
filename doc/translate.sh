facbb7787b8722396b2069a23f11f735c0d90f89
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2021-11-08 13:08:39 +0900

    docs: update

--- doc/translate.sh
+++ doc/translate.sh
@@ -4,6 +4,6 @@ BUILDDIR=_build
 SOURCEDIR=source
 
 make gettext
-cp -R ${BUILDDIR}/locale/*.pot ${SOURCEDIR}/locale/pot
-cp -R ${SOURCEDIR}/locale/pot/* ${SOURCEDIR}/locale/ja/LC_MESSAGES/
+# cp -R ${BUILDDIR}/locale/*.pot ${SOURCEDIR}/locale/pot
+# cp -R ${SOURCEDIR}/locale/pot/* ${SOURCEDIR}/locale/ja/LC_MESSAGES/
 sphinx-intl update -p ${BUILDDIR}/locale -l ja
